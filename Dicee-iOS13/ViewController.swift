ca552ad5b53cf1e31402a1715a6128c64ead7438
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-07-26 13:07:34 +0200

    Detect user interaction and respond to it Refactor completed app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,18 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        // who.what = value (image literal
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
         
+//        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
     }
-
-
+    
+    
 }
 
